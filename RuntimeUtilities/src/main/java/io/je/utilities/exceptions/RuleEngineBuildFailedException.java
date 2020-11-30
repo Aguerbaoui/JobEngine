360d719ca1a3420a1597bf6d7daa91c12a0238ab
messid <messid@integrationobjects.com>
2020-11-30 14:14:23 +0100

    added Exceptions

---
+++ RuntimeUtilities/src/main/java/io/je/utilities/exceptions/RuleEngineBuildFailedException.java
@@ -0,0 +1,15 @@
+package io.je.utilities.exceptions;
+
+public class RuleEngineBuildFailedException extends JEException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5418747468040739097L;
+
+	public RuleEngineBuildFailedException(String code, String message) {
+		super(code, message);
+		
+	}
+
+}
