a19a6d45fdeb1025f31bf312b746c5f01fd8b2d8
messid <messid@Messid-PC.integ-objects.local>
2020-12-11 10:50:30 +0100

    updated pom

--- JERunner/RuntimeManager/src/main/java/io/je/runtime/controllers/WorkflowController.java
+++ JERunner/RuntimeManager/src/main/java/io/je/runtime/controllers/WorkflowController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import builder.JEToBpmnMapper;
 import io.je.runtime.models.WorkflowModel;
 import io.je.runtime.workflow.WorkflowEngineHandler;
 import io.je.utilities.constants.Errors;
