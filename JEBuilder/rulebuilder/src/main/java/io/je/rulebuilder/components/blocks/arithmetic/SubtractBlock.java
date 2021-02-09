c9d71d2f77ea9a4d8509a3dd7527fc81beeef986
messid <messid@Messid-PC.integ-objects.local>
2021-02-09 09:19:47 +0100

    added substract block and conversion units

--- JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/blocks/arithmetic/SubtractBlock.java
+++ JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/blocks/arithmetic/SubtractBlock.java
@@ -6,40 +6,54 @@ import io.je.rulebuilder.models.BlockModel;
 
 public class SubtractBlock extends ArithmeticBlock {
 
-	String value = null;
 	public SubtractBlock(BlockModel blockModel) {
 		super(blockModel);
-		value = (blockModel.getBlockConfiguration().getValue());
+	}
+	
+	private SubtractBlock() {
+		
 	}
 
 	@Override
 	public String getExpression() {
 		StringBuilder expression = new StringBuilder();
 		expression.append("\n");
-		expression.append(inputBlocks.get(0).getExpression());
-		expression.append("\n");
-		expression.append("$"+blockName +" : Number() from " + " $"+inputBlocks.get(0).getBlockName() + " + " + value );
+		for (int i=0;i<inputBlocks.size();i++)
+		{
+			expression.append(inputBlocks.get(i).getExpression());
+			expression.append("\n");
+
+		}
+		expression.append("$"+blockName +" : Number() from " + asDouble( "$"+getInputRefName(0) ) );
+		for (int i=1;i<inputBlocks.size();i++)
+		{
+			expression.append(" - "  + asDouble( "$"+getInputRefName(i) ));
+		}
 		return expression.toString();
 	}
-
+ 
 	@Override
 	public String getAsFirstOperandExpression() {
 		StringBuilder expression = new StringBuilder();
 		expression.append("\n");
-		expression.append(inputBlocks.get(0).getExpression());
-		expression.append("\n");
-		expression.append("$"+blockName +" : Number(doubleValue " + Keywords.toBeReplaced +") from " + " $"+inputBlocks.get(0).getBlockName() + " + " + value );
+		for (int i=0;i<inputBlocks.size();i++)
+		{
+			expression.append(inputBlocks.get(i).getExpression());
+			expression.append("\n");
+
+		}
+		expression.append("$"+blockName +" : Number(doubleValue " + Keywords.toBeReplaced +") from " + asDouble( "$"+getInputRefName(0) ) );
+		for (int i=1;i<inputBlocks.size();i++)
+		{
+			expression.append(" - "  + asDouble( "$"+getInputRefName(i) ));
+		}
 		return expression.toString();
 	}
 
 	@Override
 	public String getAsSecondOperandExpression() {
-		StringBuilder expression = new StringBuilder();
-		expression.append("\n");
-		expression.append(inputBlocks.get(0).getExpression());
-		expression.append("\n");
-		expression.append("$"+blockName +" : Number( " + Keywords.toBeReplaced +" doubleValue ) from " + " $"+inputBlocks.get(0).getBlockName() + " + " + value );
-		return expression.toString();
+		return null;
+
 	}
 
 	@Override
@@ -47,7 +61,4 @@ public class SubtractBlock extends ArithmeticBlock {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
-
 }
