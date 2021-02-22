6580dfb718b276c06cf6914980903cef4a317ff4
messid <messid@Messid-PC.integ-objects.local>
2021-02-22 11:36:45 +0100

    generic unit converter added

--- RuntimeUtilities/src/main/java/io/je/utilities/math/JEConverter.java
+++ RuntimeUtilities/src/main/java/io/je/utilities/math/JEConverter.java
@@ -1,8 +1,15 @@
 package io.je.utilities.math;
 
-
+import io.je.utilities.unitconversion.JEUnitConverter;
+import io.je.utilities.unitconversion.JEUnitConverter.UnitDefinition;
 
 public class JEConverter {
+	
+	public static double convert(double value ,UnitDefinition initialUnit, UnitDefinition finalUnit)
+	{
+		 JEUnitConverter unitConverter = new JEUnitConverter(initialUnit.UNIT.getCategory() , initialUnit); 
+	        return  unitConverter.convert(value, finalUnit);
+	}
 	
 	
 	
