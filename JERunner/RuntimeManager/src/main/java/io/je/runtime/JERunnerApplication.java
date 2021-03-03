e8e237c7d87449ce0f0ae29109434094c977ece1
messid <messid@Messid-PC.integ-objects.local>
2021-03-03 11:19:38 +0100

    added runner config

--- JERunner/RuntimeManager/src/main/java/io/je/runtime/JERunnerApplication.java
+++ JERunner/RuntimeManager/src/main/java/io/je/runtime/JERunnerApplication.java
@@ -2,19 +2,24 @@ package io.je.runtime;
 
 import java.util.Collections;
 
+import io.je.project.config.ConfigurationConstants;
 import io.je.utilities.apis.JEBuilderApiHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
 
 import io.je.utilities.logger.JELogger;
 
 import javax.annotation.PreDestroy;
-
+import io.je.runtime.config.ConfigurationConstants;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@PropertySource(ConfigurationConstants.APPLICATION_PROPERTIES_PATH)
 public class JERunnerApplication {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(JERunnerApplication.class, args);
     }
