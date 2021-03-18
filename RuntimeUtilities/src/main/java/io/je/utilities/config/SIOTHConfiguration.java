4aa3284e1fa757dd792da58dac2570aea1696058
messid <messid@Messid-PC.integ-objects.local>
2021-03-18 11:30:55 +0100

    removed mongo config from properties file

---
+++ RuntimeUtilities/src/main/java/io/je/utilities/config/SIOTHConfiguration.java
@@ -0,0 +1,111 @@
+package io.je.utilities.config;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.Response;
+
+import io.je.utilities.constants.ResponseCodes;
+import io.je.utilities.exceptions.JEException;
+
+public class SIOTHConfiguration {
+
+	private SIOTHConfiguration instance;
+	private static String mongoServerHostName;
+	private static String mongoServerPort;
+	private static String mongoUserName;
+	private static String mongoPassword;
+	private static String dataBaseName;
+
+	private SIOTHConfiguration() {
+		super();
+	}
+
+	public static void init() {
+		String jsonString = loadSIOTHConfig();
+		if (jsonString != null) {
+			JSONObject jsonConfig = new JSONObject(jsonString);
+			JSONObject mongoConfig = jsonConfig.getJSONObject(ConfigurationConstants.mongoConfiguration);
+			mongoServerHostName = mongoConfig.getString(ConfigurationConstants.mongoServerHostName);
+			mongoServerPort =String.valueOf( mongoConfig.getInt(ConfigurationConstants.mongoServerPort));
+			mongoUserName = mongoConfig.getString(ConfigurationConstants.mongoUserName);
+			mongoPassword = mongoConfig.getString(ConfigurationConstants.mongoPassword);
+			dataBaseName="JobEngine";
+
+		}
+	}
+
+	private static String loadSIOTHConfig() {
+		String configPath = ConfigurationConstants.SIOTH_JSON_CONFIG;
+		try {
+			String file = configPath;
+			String json = new String(Files.readAllBytes(Paths.get(file)));
+			return json;
+
+		} catch (Exception e) {
+			System.out.println("Failed to start application. [SIOTHConfig.json] was not found in : " + configPath);
+			return null;
+		}
+
+	}
+
+	public static String getConfig(String configName) {
+		return null;
+	}
+
+	public SIOTHConfiguration getInstance() {
+		if (instance == null) {
+			instance = new SIOTHConfiguration();
+		}
+		return instance;
+	}
+
+	public static String getMongoServerHostName() {
+		return mongoServerHostName;
+	}
+
+	public static void setMongoServerHostName(String mongoServerHostName) {
+		SIOTHConfiguration.mongoServerHostName = mongoServerHostName;
+	}
+
+	public static String getMongoServerPort() {
+		return mongoServerPort;
+	}
+
+	public static void setMongoServerPort(String mongoServerPort) {
+		SIOTHConfiguration.mongoServerPort = mongoServerPort;
+	}
+
+	public static String getMongoUserName() {
+		return mongoUserName;
+	}
+
+	public static void setMongoUserName(String mongoUserName) {
+		SIOTHConfiguration.mongoUserName = mongoUserName;
+	}
+
+	public static String getMongoPassword() {
+		return mongoPassword;
+	}
+
+	public static void setMongoPassword(String mongoPassword) {
+		SIOTHConfiguration.mongoPassword = mongoPassword;
+	}
+
+	public static String getDataBaseName() {
+		return dataBaseName;
+	}
+
+	private static void setDataBaseName(String dataBaseName) {
+		SIOTHConfiguration.dataBaseName = dataBaseName;
+	}
+	
+			
+
+}
