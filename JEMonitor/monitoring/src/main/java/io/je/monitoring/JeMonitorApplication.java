86e0c652009c04fb9a8b2cfaccdf7aa24c0a8b37
messid <messid@Messid-PC.integ-objects.local>
2021-03-23 14:33:01 +0100

    added monitoring project

---
+++ JEMonitor/monitoring/src/main/java/io/je/monitoring/JeMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.je.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeMonitorApplication.class, args);
+	}
+
+}
---
+++ JEMonitor/monitoring/src/test/java/io/je/monitoring/JeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.je.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
