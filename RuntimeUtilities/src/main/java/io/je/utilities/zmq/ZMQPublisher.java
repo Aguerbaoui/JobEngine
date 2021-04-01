60a6e3b62acc398080f48adf126b9a1b41ef67e7
messid <messid@Messid-PC.integ-objects.local>
2021-04-01 13:36:43 +0200

    added zmq publisher

---
+++ RuntimeUtilities/src/main/java/io/je/utilities/zmq/ZMQPublisher.java
@@ -0,0 +1,41 @@
+package io.je.utilities.zmq;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+
+public class ZMQPublisher {
+
+	protected  ZContext context;
+	protected Socket socket;
+	protected String url;
+	protected int publishPort;
+	private String connectionUrl;
+
+
+	public ZMQPublisher(String url,int publishPort) {
+		
+		this.url = url;
+		this.publishPort=publishPort;
+		this.connectionUrl = url + ":" + publishPort;
+		context = new ZContext();
+		socket = context.createSocket(SocketType.PUB);
+		socket.connect(connectionUrl);
+	}
+
+	public void publish(String msgToBePublished,String topic) {
+		socket.sendMore(topic);
+		socket.send( msgToBePublished,0);
+	}
+
+	public void open() {
+		context = new ZContext();
+		socket = context.createSocket(SocketType.PUB);
+		socket.connect(connectionUrl);
+	}
+
+	public void close() {
+		socket.close();
+		context.close();
+	}
+}
