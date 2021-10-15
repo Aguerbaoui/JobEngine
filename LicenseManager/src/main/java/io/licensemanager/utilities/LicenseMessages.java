8e28c906fe7cd8886d5fa7e96287fd964fd53d56
Manel Essid <messid@integrationobjects.com>
2021-10-15 12:15:05 +0100

    remove hardcoded messages : license Manager.

---
+++ LicenseManager/src/main/java/io/licensemanager/utilities/LicenseMessages.java
@@ -0,0 +1,29 @@
+package io.licensemanager.utilities;
+
+public class LicenseMessages {
+
+	
+	private LicenseMessages()
+	{
+		
+	}
+	
+	public static final  String ERROR_RECEIVING_DATA = "Error occurred while receiving data.";
+	public static final  String ERROR_SENDING_REQUEST = "Error occurred while sending request.";
+
+	public static String licenseManagerUnreachable(String licenseManagerUrl )
+	{
+		return "The License Manager is not reachable. License Manager Address: "+licenseManagerUrl+".";
+	}
+	
+	public static String initZMQError(String strError)
+	{
+		return "Error occurred while initializing ZMQ Request. Error: " + strError +".";
+	}
+
+
+	public static String initLicenseError(String strError)
+	{
+		return "Exception occurred while initializing license. Exception: "  + strError+".";
+	}
+}
