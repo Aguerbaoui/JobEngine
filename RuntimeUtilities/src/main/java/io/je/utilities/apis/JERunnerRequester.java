d84fd3df71f6560369f2334a635830acb2b68857
Manel Essid <messid@integrationobjects.com>
2022-04-11 11:26:31 +0100

    added response manager

--- RuntimeUtilities/src/main/java/io/je/utilities/apis/JERunnerRequester.java
+++ RuntimeUtilities/src/main/java/io/je/utilities/apis/JERunnerRequester.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.je.utilities.beans.JEZMQResponse;
 import io.je.utilities.beans.RunnerRequestEnum;
 import io.je.utilities.beans.RunnerRequestObject;
+import io.je.utilities.beans.ZMQResponseType;
 import io.je.utilities.log.JELogger;
 import io.je.utilities.mapping.VariableModelMapping;
 import io.siothconfig.SIOTHConfigUtility;
@@ -13,55 +16,61 @@ import utils.zmq.ZMQRequester;
 
 public class JERunnerRequester {
 
-		
-		static int requesterPort=59095;
-		
-	
-	    private static ZMQRequester requester;
-	    private static ObjectMapper objectMapper = new ObjectMapper();
-	    
-	    private static void init()
-	    {
-	    	try {
-		    	requester = new ZMQRequester("tcp://" +SIOTHConfigUtility.getSiothConfig().getNodes().getSiothMasterNode(),requesterPort );
+	static int requesterPort = 59095;
 
-	    	}catch (Exception e) {
-			}
-	    }
-	    
-	    
-	    public static boolean updateVariable(String projectId,String variableId, Object value, boolean ignoreIfSameValue)
-	    {
-	    	RunnerRequestObject request = new RunnerRequestObject(RunnerRequestEnum.UPDATE_VARIABLE);
-	    	Map<String,Object> requestParams= new HashMap<>();
-	    	requestParams.put(VariableModelMapping.VARIABLE_ID,variableId);
-	    	requestParams.put(VariableModelMapping.PROJECT_ID, projectId);
-	    	requestParams.put(VariableModelMapping.VALUE, value);
-	    	requestParams.put(VariableModelMapping.IGNORE_IF_SAME_VALUE, ignoreIfSameValue);
-	    	request.setRequestBody(requestParams);
-	    	return sendRequest(request);
-	    }
-	    
-	    
-	    private static boolean sendRequest(RunnerRequestObject request)
-	    {
-	    	try {
-    			if(requester==null) init();
-	    		synchronized (requester) {
-				requester.sendRequest(objectMapper.writeValueAsString(request));	
-				}
-	    		
-	    		return true;
-	    	}catch (Exception e) {
-				return false;
-			}
-	    }
+	private static ZMQRequester requester;
+	private static ObjectMapper objectMapper = new ObjectMapper();
 
+	private static void init() {
+		try {
+			requester = new ZMQRequester("tcp://" + SIOTHConfigUtility.getSiothConfig().getNodes().getSiothMasterNode(),
+					requesterPort);
 
-		public static void setRequesterPort(int requesterPort) {
-			JERunnerRequester.requesterPort = requesterPort;
+		} catch (Exception e) {
 		}
-	    
-	    
-	    
+	}
+
+	public static JEZMQResponse updateVariable(String projectId, String variableId, Object value,
+			boolean ignoreIfSameValue) {
+		RunnerRequestObject request = new RunnerRequestObject(RunnerRequestEnum.UPDATE_VARIABLE);
+		Map<String, Object> requestParams = new HashMap<>();
+		requestParams.put(VariableModelMapping.VARIABLE_ID, variableId);
+		requestParams.put(VariableModelMapping.PROJECT_ID, projectId);
+		requestParams.put(VariableModelMapping.VALUE, value);
+		requestParams.put(VariableModelMapping.IGNORE_IF_SAME_VALUE, ignoreIfSameValue);
+		request.setRequestBody(requestParams);
+		return sendRequest(request);
+	}
+
+	public static JEZMQResponse readVariable(String projectId, String variableId) {
+		RunnerRequestObject request = new RunnerRequestObject(RunnerRequestEnum.GET_VARIABLE);
+		Map<String, Object> requestParams = new HashMap<>();
+		requestParams.put(VariableModelMapping.VARIABLE_ID, variableId);
+		requestParams.put(VariableModelMapping.PROJECT_ID, projectId);
+		request.setRequestBody(requestParams);
+		return sendRequest(request);
+	}
+
+	private static JEZMQResponse sendRequest(RunnerRequestObject request)
+	    {
+	    	try {
+	    		String response;
+	    		if(requester==null) init();
+	    		synchronized (requester) {
+	    			response = requester.sendRequest(objectMapper.writeValueAsString(request));	
+	  	    	}
+	    		
+				return objectMapper.readValue(response, JEZMQResponse.class);				
+	    	}catch (Exception e) {
+	    			e.printStackTrace();
+					return new JEZMQResponse(ZMQResponseType.FAIL,"Failed to get response from JERunner.");
+
+	    		}
+			
+	    }
+
+	public static void setRequesterPort(int requesterPort) {
+		JERunnerRequester.requesterPort = requesterPort;
+	}
+
 }
