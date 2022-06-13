2ac9bdd8bf150f6d3b9b33c07cbecace62fe8a32
hattia <hattia@integrationobjects.com>
2022-06-13 13:53:25 +0100

    ref: remove debug log from get all events

--- JEBuilder/ProjectBuilder/src/main/java/io/je/project/controllers/EventController.java
+++ JEBuilder/ProjectBuilder/src/main/java/io/je/project/controllers/EventController.java
@@ -24,146 +24,145 @@ import java.util.List;
 public class EventController {
 
 
-	@Autowired
-	EventService eventService;
+    @Autowired
+    EventService eventService;
 
-	@Autowired
-	ProjectService projectService;
+    @Autowired
+    ProjectService projectService;
 
-	/*
-	 * Retrieve all events in a project
-	 */
-	@GetMapping(value = "{projectId}/getAllEvents")
-	@ResponseBody
-	public ResponseEntity<?> getAllEvents(@PathVariable("projectId") String projectId) {
-		Collection<?> events = null;
-		System.out.println("aaa");
-		try {
-			projectService.getProject(projectId);
+    /*
+     * Retrieve all events in a project
+     */
+    @GetMapping(value = "{projectId}/getAllEvents")
+    @ResponseBody
+    public ResponseEntity<?> getAllEvents(@PathVariable("projectId") String projectId) {
+        Collection<?> events = null;
+        try {
+            projectService.getProject(projectId);
 
-			events = eventService.getAllEvents(projectId);
+            events = eventService.getAllEvents(projectId);
 			/*if (events.isEmpty()) {
 				return ResponseEntity.noContent().build();
 
 			}*/
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
 
-		}
+        }
 
-		return ResponseEntity.ok(events);
+        return ResponseEntity.ok(events);
 
-	}
+    }
 
-	/*
-	 * Retrieve an event from a project
-	 */
-	@GetMapping(value = "{projectId}/getEvent/{eventId}")
-	@ResponseBody
-	public ResponseEntity<?> getEvent(@PathVariable("projectId") String projectId,
-									  @PathVariable("eventId") String eventId) {
-		EventModel event = null;
+    /*
+     * Retrieve an event from a project
+     */
+    @GetMapping(value = "{projectId}/getEvent/{eventId}")
+    @ResponseBody
+    public ResponseEntity<?> getEvent(@PathVariable("projectId") String projectId,
+                                      @PathVariable("eventId") String eventId) {
+        EventModel event = null;
 
-		try {
-			projectService.getProject(projectId);
-			event = eventService.getEvent(projectId, eventId);
-			if (event == null) {
-				return ResponseEntity.noContent()
-						.build();
+        try {
+            projectService.getProject(projectId);
+            event = eventService.getEvent(projectId, eventId);
+            if (event == null) {
+                return ResponseEntity.noContent()
+                        .build();
 
-			}
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
+            }
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
 
-		}
+        }
 
-		return ResponseEntity.ok(event);
+        return ResponseEntity.ok(event);
 
-	}
+    }
 
-	/*
-	 * add event
-	 */
-	@PostMapping(value = "/{projectId}/addEvent", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> addEvent(@PathVariable("projectId") String projectId, @RequestBody EventModel eventModel) {
+    /*
+     * add event
+     */
+    @PostMapping(value = "/{projectId}/addEvent", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addEvent(@PathVariable("projectId") String projectId, @RequestBody EventModel eventModel) {
 
-		try {
-			projectService.getProject(projectId);
+        try {
+            projectService.getProject(projectId);
 
-			eventService.addEvent(projectId, eventModel);
+            eventService.addEvent(projectId, eventModel);
 
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
 
-		}
-		return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_ADDED));
-	}
-
-
-	/*
-	 * update event
-	 */
-	@PostMapping(value = "/{projectId}/updateEvent", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> updateEvent(@PathVariable("projectId") String projectId, @RequestBody EventModel eventModel) {
-
-		try {
-			projectService.getProject(projectId);
-
-			eventService.updateEvent(projectId, eventModel);
-
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
-
-		}
-		return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_UPDATED));
-	}
+        }
+        return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_ADDED));
+    }
 
 
-	/*
-	 * delete event
-	 */
-	@DeleteMapping(value = "{projectId}/deleteEvent/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> deleteEvent(@PathVariable("projectId") String projectId,
-										 @PathVariable("eventId") String eventId) {
+    /*
+     * update event
+     */
+    @PostMapping(value = "/{projectId}/updateEvent", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateEvent(@PathVariable("projectId") String projectId, @RequestBody EventModel eventModel) {
 
-		try {
-			projectService.getProject(projectId);
-			eventService.deleteEvent(projectId, eventId);
+        try {
+            projectService.getProject(projectId);
 
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
+            eventService.updateEvent(projectId, eventModel);
 
-		}
-		return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_DELETED));
-	}
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
 
-	/*
-	 * delete multiple events
-	 */
-	@PostMapping(value = "/deleteEvents/{projectId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> deleteEvents(@PathVariable("projectId") String projectId, @RequestBody(required = false) List<String> ids) {
-
-		try {
-			projectService.getProject(projectId);
-			eventService.deleteEvents(projectId, ids);
-		} catch (Exception e) {
-			return JEExceptionHandler.handleException(e);
-		}
-		return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENTS_DELETED));
-	}
+        }
+        return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_UPDATED));
+    }
 
 
+    /*
+     * delete event
+     */
+    @DeleteMapping(value = "{projectId}/deleteEvent/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteEvent(@PathVariable("projectId") String projectId,
+                                         @PathVariable("eventId") String eventId) {
 
+        try {
+            projectService.getProject(projectId);
+            eventService.deleteEvent(projectId, eventId);
 
-	/*
-	 * to be deleted
-	 */
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
+
+        }
+        return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENT_DELETED));
+    }
+
+    /*
+     * delete multiple events
+     */
+    @PostMapping(value = "/deleteEvents/{projectId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteEvents(@PathVariable("projectId") String projectId, @RequestBody(required = false) List<String> ids) {
+
+        try {
+            projectService.getProject(projectId);
+            eventService.deleteEvents(projectId, ids);
+        } catch (Exception e) {
+            return JEExceptionHandler.handleException(e);
+        }
+        return ResponseEntity.ok(new JEResponse(ResponseCodes.CODE_OK, JEMessages.EVENTS_DELETED));
+    }
 
 
 
-	/*
-	 * trigger event
-	 */
+
+    /*
+     * to be deleted
+     */
+
+
+
+    /*
+     * trigger event
+     */
 	/*@GetMapping(value = "/triggerEvent/{projectId}/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> triggerEvent(@PathVariable("projectId") String projectId, @PathVariable("eventId") String eventId) {
 
