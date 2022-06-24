254f7837b9b52e4b4c1787159eebca9ce07d9e78
hattia <hattia@integrationobjects.com>
2022-06-24 11:04:26 +0100

    fix: fix issue with instances topics not emptying its values after update

--- JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/JERule.java
+++ JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/JERule.java
@@ -1,204 +1,181 @@
 package io.je.rulebuilder.components;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
+import io.je.utilities.beans.Status;
+import io.je.utilities.runtimeobject.JEObject;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import io.je.utilities.beans.Status;
-import io.je.utilities.runtimeobject.JEObject;
+import java.util.HashMap;
+import java.util.Map;
 
-@Document(collection="JERuleCollection")
-public abstract class JERule extends JEObject  {
-	
-
-	/*
-	 * buildStatus
-	 */
-	boolean isBuilt = false;
-	
-	
-	boolean isCompiled = false;
-	
-	/*
-	 * check if rule was added to JERunner or not
-	 */
-	boolean isAdded =  false;
-	
-	boolean isRunning = false;
-	
-	boolean enabled = true;
-	
-	@Transient
-	boolean containsErrors = false;
-	 
-	
-	Status status = Status.NOT_BUILT;
-	
-	@Transient
-	private Map<String,Integer> topics = new HashMap<>();
-	
-	protected Map<String,Integer> classTopics = new HashMap<>();
-	
-	protected Map<String,Integer> instanceTopics = new HashMap<>();
-	
-	String description ;
-	
-	String ruleFrontConfig;
-	
-	
-	
-	public JERule(String jobEngineElementID, String jobEngineProjectID, String ruleName) {
-		super(jobEngineElementID, jobEngineProjectID, ruleName);
-	}
-	
-	public JERule(String jobEngineElementID, String jobEngineProjectID, String ruleName,String projectName) {
-		super(jobEngineElementID, jobEngineProjectID, ruleName,projectName);
-	}
+@Document(collection = "JERuleCollection")
+public abstract class JERule extends JEObject {
 
 
-	public JERule() {
-	}
-
-	public boolean isBuilt() {
-		return isBuilt;
-	}
+    /*
+     * buildStatus
+     */
+    boolean isBuilt = false;
 
 
-	
-	
-	public boolean isAdded() {
-		return isAdded;
-	}
+    boolean isCompiled = false;
+
+    /*
+     * check if rule was added to JERunner or not
+     */
+    boolean isAdded = false;
+
+    boolean isRunning = false;
+
+    boolean enabled = true;
+
+    @Transient
+    boolean containsErrors = false;
 
 
+    Status status = Status.NOT_BUILT;
 
-	public void setAdded(boolean isAdded) {
-		this.isAdded = isAdded;
-	}
+    @Transient
+    private Map<String, Integer> topics = new HashMap<>();
 
-	
+    //protected Map<String,Integer> classTopics = new HashMap<>();
 
-	public boolean isCompiled() {
-		return isCompiled;
-	}
+    protected Map<String, Integer> instanceTopics = new HashMap<>();
 
-	public void setCompiled(boolean isCompiled) {
-		this.isCompiled = isCompiled;
-	}
+    String description;
 
-	public void setBuilt(boolean isBuilt) {
-		if(!isBuilt)
-		{
-			this.isCompiled = false;
-		}
-		this.isBuilt = isBuilt;
-		
-	}
-
-	public String getRuleFrontConfig() {
-		return ruleFrontConfig;
-	}
-
-	public void setRuleFrontConfig(String ruleFrontConfig) {
-		this.ruleFrontConfig = ruleFrontConfig;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void addTopic(String topic,Map<String,Integer> topics) {
-		if(topic!=null)
-		{
-			if(!topics.containsKey(topic))
-			{
-				topics.put(topic,1);
-			}
-			else {
-				topics.put(topic,topics.get(topic)+1);
-			}
-		}
-
-	}
-	
-	
-	public void updateTopic(String oldTopic, String newTopic,Map<String,Integer> topics) {
-			removeTopic(oldTopic,topics);
-			addTopic(newTopic,topics);
-	}
-	
-	public void removeTopic(String topic,Map<String,Integer> topics)
-	{
-		if(topics.containsKey(topic))
-		{
-			topics.put(topic,topics.get(topic)-1);
-			if(topics.get(topic)==0)
-			{
-				topics.remove(topic);
-			}
-		}
-	}
-
-	public Map<String, Integer> getTopics() {
-		return topics;
-	}
-
-	public void setTopics(Map<String, Integer> topics) {
-		this.topics = topics;
-	}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public boolean isRunning() {
-		return isRunning;
-	}
-
-	public void setRunning(boolean isRunning) {
-		this.isRunning = isRunning;
-	}
-
-	public Status getStatus() {
-		return status;
-	}
-
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-
-	public void loadTopics() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean containsErrors() {
-		return containsErrors;
-	}
-
-	public void setContainsErrors(boolean containsErrors) {
-		this.containsErrors = containsErrors;
-	}
+    String ruleFrontConfig;
 
 
-	
-		
+    public JERule(String jobEngineElementID, String jobEngineProjectID, String ruleName) {
+        super(jobEngineElementID, jobEngineProjectID, ruleName);
+    }
+
+    public JERule(String jobEngineElementID, String jobEngineProjectID, String ruleName, String projectName) {
+        super(jobEngineElementID, jobEngineProjectID, ruleName, projectName);
+    }
 
 
+    public JERule() {
+    }
+
+    public boolean isBuilt() {
+        return isBuilt;
+    }
 
 
-	
-	
+    public boolean isAdded() {
+        return isAdded;
+    }
+
+
+    public void setAdded(boolean isAdded) {
+        this.isAdded = isAdded;
+    }
+
+
+    public boolean isCompiled() {
+        return isCompiled;
+    }
+
+    public void setCompiled(boolean isCompiled) {
+        this.isCompiled = isCompiled;
+    }
+
+    public void setBuilt(boolean isBuilt) {
+        if (!isBuilt) {
+            this.isCompiled = false;
+        }
+        this.isBuilt = isBuilt;
+
+    }
+
+    public String getRuleFrontConfig() {
+        return ruleFrontConfig;
+    }
+
+    public void setRuleFrontConfig(String ruleFrontConfig) {
+        this.ruleFrontConfig = ruleFrontConfig;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void addTopic(String topic, Map<String, Integer> topics) {
+        if (topic != null) {
+            if (!topics.containsKey(topic)) {
+                topics.put(topic, 1);
+            } else {
+                topics.put(topic, topics.get(topic) + 1);
+            }
+        }
+
+    }
+
+
+    public void updateTopic(String oldTopic, String newTopic, Map<String, Integer> topics) {
+        removeTopic(oldTopic, topics);
+        addTopic(newTopic, topics);
+    }
+
+    public void removeTopic(String topic, Map<String, Integer> topics) {
+        if (topics.containsKey(topic)) {
+            topics.put(topic, topics.get(topic) - 1);
+            if (topics.get(topic) == 0) {
+                topics.remove(topic);
+            }
+        }
+    }
+
+    public Map<String, Integer> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(Map<String, Integer> topics) {
+        this.topics = topics;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void loadTopics() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean containsErrors() {
+        return containsErrors;
+    }
+
+    public void setContainsErrors(boolean containsErrors) {
+        this.containsErrors = containsErrors;
+    }
+
 
 }
--- JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/UserDefinedRule.java
+++ JEBuilder/rulebuilder/src/main/java/io/je/rulebuilder/components/UserDefinedRule.java
@@ -1,165 +1,153 @@
 package io.je.rulebuilder.components;
 
+import io.je.rulebuilder.components.blocks.Block;
+import io.je.rulebuilder.components.blocks.getter.InstanceGetterBlock;
+import org.springframework.data.annotation.Transient;
+
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.data.annotation.Transient;
-
-import io.je.rulebuilder.components.blocks.Block;
-import io.je.rulebuilder.components.blocks.getter.InstanceGetterBlock;
-
 /*
  * Rules defined graphically by the user.
- * One UserDefinedRule can be equivalents to multiple drl rules 
+ * One UserDefinedRule can be equivalents to multiple drl rules
  * Each Job engine rule is defined by a root block ( a logic or comparison block that precedes and execution sequence)
  */
 public class UserDefinedRule extends JERule {
 
-	/*
-	 * rule attributes
-	 */
-	RuleParameters ruleParameters;
+    /*
+     * rule attributes
+     */
+    RuleParameters ruleParameters;
 
-	/*
-	 * Map of all the blocks that define this rule
-	 */
-	BlockManager blocks = new BlockManager();
+    /*
+     * Map of all the blocks that define this rule
+     */
+    BlockManager blocks = new BlockManager();
 
-	/*
-	 * * One UserDefinedRule can be equivalents to multiple drl rules List of all
-	 * the subRules
-	 */
+    /*
+     * * One UserDefinedRule can be equivalents to multiple drl rules List of all
+     * the subRules
+     */
 
-	@Transient
-	List<ScriptedRule> unitRules = new ArrayList<ScriptedRule>();
-	
-	List<String> subRules = new ArrayList<String>();
+    @Transient
+    List<ScriptedRule> unitRules = new ArrayList<ScriptedRule>();
 
-	public UserDefinedRule() {
-		ruleParameters = new RuleParameters();
-	}
+    List<String> subRules = new ArrayList<String>();
 
-	
+    public UserDefinedRule() {
+        ruleParameters = new RuleParameters();
+    }
 
-	/*
-	 * add a block to this user defined rule
-	 */
-	public void addBlock(Block block) {
-		blocks.addBlock(block);
-		isBuilt = false;
-	}
 
-	/*
-	 * update a block in this user defined rule
-	 */
-	public void updateBlock(Block block) {
-		blocks.updateBlock(block);
-		isBuilt = false;
+    /*
+     * add a block to this user defined rule
+     */
+    public void addBlock(Block block) {
+        blocks.addBlock(block);
+        isBuilt = false;
+    }
 
-	}
-	
+    /*
+     * update a block in this user defined rule
+     */
+    public void updateBlock(Block block) {
+        blocks.updateBlock(block);
+        isBuilt = false;
 
-	
-	//Topics are class ids or classId#instanceId for specific instances
-	//if a block has not specific instances, it means it is to be applied on all instances 
-	@Override
-	public void loadTopics()
-	{
-		this.setTopics(new ConcurrentHashMap());
-		for(Block block : blocks.getAll())
-		{
-			
-			
-			if(block instanceof InstanceGetterBlock)
-			{
-				InstanceGetterBlock b = (InstanceGetterBlock)block;
-				if(b.getSpecificInstances().isEmpty())
-				{
-					addTopic(b.getClassId(),this.getTopics());
-				}else
-				{
-					for (String instanceId : b.getSpecificInstances())
-					{
-						addTopic(b.getClassId()+"#"+instanceId,instanceTopics);
-					}
-				}
-			}
-		}
-		
-		//for specific instance topics, they are only added if there isn't already a generic topic that inludes their class
-		for(String instanceTopic : instanceTopics.keySet())
-		{
-			if(!this.getTopics().containsKey(instanceTopic.split("#")[0])) {
-				addTopic(instanceTopic,this.getTopics());
-			}
-		}
-		
-	}
-	
-	
+    }
 
-	/*
-	 * delete a block in this user defined rule
-	 */
-	public void deleteBlock(String blockId) {
+
+    //Topics are class ids or classId#instanceId for specific instances
+    //if a block has not specific instances, it means it is to be applied on all instances
+    @Override
+    public void loadTopics() {
+        this.setTopics(new ConcurrentHashMap());
+        instanceTopics = new ConcurrentHashMap<>();
+        for (Block block : blocks.getAll()) {
+
+
+            if (block instanceof InstanceGetterBlock) {
+                InstanceGetterBlock b = (InstanceGetterBlock) block;
+                if (b.getSpecificInstances()
+                        .isEmpty()) {
+                    addTopic(b.getClassId(), this.getTopics());
+                } else {
+                    for (String instanceId : b.getSpecificInstances()) {
+                        addTopic(b.getClassId() + "#" + instanceId, instanceTopics);
+                    }
+                }
+            }
+        }
+
+        //for specific instance topics, they are only added if there isn't already a generic topic that includes their class
+        for (String instanceTopic : instanceTopics.keySet()) {
+            if (!this.getTopics()
+                    .containsKey(instanceTopic.split("#")[0])) {
+                addTopic(instanceTopic, this.getTopics());
+            }
+        }
+
+    }
+
+
+    /*
+     * delete a block in this user defined rule
+     */
+    public void deleteBlock(String blockId) {
 		/*if (blocks.getBlock(blockId) instanceof AttributeGetterBlock) {
 			AttributeGetterBlock getter = (AttributeGetterBlock) blocks.getBlock(blockId);
 			removeTopic(getter.getClassId());
 		}*/
-		blocks.deleteBlock(blockId);
-		isBuilt = false;
+        blocks.deleteBlock(blockId);
+        isBuilt = false;
 
-	}
+    }
 
-	public RuleParameters getRuleParameters() {
-		return ruleParameters;
-	}
+    public RuleParameters getRuleParameters() {
+        return ruleParameters;
+    }
 
-	public void setRuleParameters(RuleParameters ruleParameters) {
-		this.ruleParameters = ruleParameters;
-		isBuilt = false;
+    public void setRuleParameters(RuleParameters ruleParameters) {
+        this.ruleParameters = ruleParameters;
+        isBuilt = false;
 
-	}
+    }
 
-	public BlockManager getBlocks() {
-		return blocks;
-	}
+    public BlockManager getBlocks() {
+        return blocks;
+    }
 
-	public Enumeration<String> getAllBlockIds()
-	{
-		return blocks.getAllBlockIds();
-	}
-	
-	public void setBlocks(BlockManager blocks) {
-		this.blocks = blocks;
-	}
+    public Enumeration<String> getAllBlockIds() {
+        return blocks.getAllBlockIds();
+    }
 
-	public List<String> getSubRules() {
-		return subRules;
-	}
+    public void setBlocks(BlockManager blocks) {
+        this.blocks = blocks;
+    }
 
-	public void setSubRules(List<String> subRules) {
-		this.subRules = subRules;
-	}
+    public List<String> getSubRules() {
+        return subRules;
+    }
 
-	public boolean containsBlock(String blockId) {
-		return blocks.containsBlock(blockId);
-	}
+    public void setSubRules(List<String> subRules) {
+        this.subRules = subRules;
+    }
 
+    public boolean containsBlock(String blockId) {
+        return blocks.containsBlock(blockId);
+    }
 
 
-	public List<ScriptedRule> getUnitRules() {
-		return unitRules;
-	}
+    public List<ScriptedRule> getUnitRules() {
+        return unitRules;
+    }
 
 
+    public void setUnitRules(List<ScriptedRule> unitRules) {
+        this.unitRules = unitRules;
+    }
 
-	public void setUnitRules(List<ScriptedRule> unitRules) {
-		this.unitRules = unitRules;
-	}
 
-	
-	
 }
