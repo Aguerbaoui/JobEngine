aef19a693d582883ca1e78a32d5366b68f266d10
kzouali <kzouali@integrationobjects.com>
2022-08-28 15:20:40 +0100

    Add mocking for static methods

--- JEBuilder/ProjectBuilder/src/test/java/io/je/UnitTest.java
+++ JEBuilder/ProjectBuilder/src/test/java/io/je/UnitTest.java
@@ -2,9 +2,11 @@ package io.je;
 
 import io.je.project.config.LicenseProperties;
 import io.je.project.controllers.WorkflowController;
+import io.je.utilities.log.JELogger;
 import org.junit.After;
 import org.junit.Before;
 import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +14,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import utils.log.LogCategory;
+import utils.log.LogSubModule;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("unit-test")
 public class UnitTest {
+
+    @InjectMocks
+    LicenseProperties licenseProperties;
 
     @Autowired
     public MockMvc mockMvc;
@@ -28,9 +35,13 @@ public class UnitTest {
     public void openMocks() {
         closeable = MockitoAnnotations.openMocks(this);
 
-        Mockito.mockStatic(LicenseProperties.class).when(() -> LicenseProperties.init()).thenReturn(null);
+        try (MockedStatic<LicenseProperties> licenseProperties = Mockito.mockStatic(LicenseProperties.class)) {
 
-        Mockito.mockStatic(LicenseProperties.class).when(() -> LicenseProperties.licenseIsActive()).thenReturn(true);
+            licenseProperties.when(LicenseProperties::init).thenReturn("No License check for Unit Tests");
+
+            licenseProperties.when(LicenseProperties::licenseIsActive).thenReturn(true);
+
+        }
 
     }
 
