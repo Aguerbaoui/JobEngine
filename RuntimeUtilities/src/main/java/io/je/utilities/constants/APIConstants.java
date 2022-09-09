43ff45c655ff1045bb662f7a92ead9aa972efff1
hattia <hattia@integrationobjects.com>
2022-09-09 15:01:21 +0100

    feat: add access to blocks values in email(make it generic in Block)

--- RuntimeUtilities/src/main/java/io/je/utilities/constants/APIConstants.java
+++ RuntimeUtilities/src/main/java/io/je/utilities/constants/APIConstants.java
@@ -5,77 +5,77 @@ public class APIConstants {
 
     // public  static String RUNTIME_MANAGER_BASE_API = "http://127.0.0.1:8182/RuntimeManager-0.0.1.war";
 
-    public final static String ADD_TOPIC = "/project/addTopics";
+    public final static String ADD_TOPIC = "project/addTopics";
 
-    public final static String ADD_WORKFLOW = "/workflow/addWorkflow";
+    public final static String ADD_WORKFLOW = "workflow/addWorkflow";
 
-    public final static String RUN_WORKFLOW = "/workflow/runWorkflow/";
+    public final static String RUN_WORKFLOW = "workflow/runWorkflow/";
 
-    public final static String UPDATE_WORKFLOW_STATUS = "/workflow/updateStatus";
+    public final static String UPDATE_WORKFLOW_STATUS = "workflow/updateStatus";
 
-    public final static String ADD_VARIABLE = "/variable/addVariable/";
+    public final static String ADD_VARIABLE = "variable/addVariable/";
 
-    public final static String ADD_JAR = "/uploadJar";
+    public final static String ADD_JAR = "uploadJar";
 
-    public final static String DELETE_VARIABLE = "/variable/deleteVariable/";
+    public final static String DELETE_VARIABLE = "variable/deleteVariable/";
 
 
-    public final static String COMPILE_RULE = "/rule/compileRule";
+    public final static String COMPILE_RULE = "rule/compileRule";
 
 
     public static final int SUBSCRIBER_TIMEOUT = 1000;
 
     public static final int REQUESTER_TIMEOUT = 5000;
 
-    public static final String ADD_RULE = "/rule/addRule";
+    public static final String ADD_RULE = "rule/addRule";
 
-    public static final String UPDATE_RULE = "/rule/updateRule";
+    public static final String UPDATE_RULE = "rule/updateRule";
 
-    public static final String ADD_CLASS = "/addClass";
+    public static final String ADD_CLASS = "addClass";
 
-    public static final String UPDATE_CLASS = "/updateClass";
+    public static final String UPDATE_CLASS = "updateClass";
 
-    public static final String ADD_CLASSES = "/addClasses";
+    public static final String ADD_CLASSES = "addClasses";
 
-    public static final String DELETE_WORKFLOW = "/workflow/deleteWorkflow";
+    public static final String DELETE_WORKFLOW = "workflow/deleteWorkflow";
 
-    public static final String RUN_PROJECT = "/project/runProject/";
+    public static final String RUN_PROJECT = "project/runProject/";
 
-    public static final String CLEAN_HOUSE = "/project/removeProjectData/";
+    public static final String CLEAN_HOUSE = "project/removeProjectData/";
 
-    public static final String STOP_PROJECT = "/project/stopProject/";
+    public static final String STOP_PROJECT = "project/stopProject/";
 
     public static final String DEFAULT = "DEFAULT";
 
-    public static final String DELETE_RULE = "/rule/deleteRule";
+    public static final String DELETE_RULE = "rule/deleteRule";
 
-    public static final String ACTUATOR_HEALTH = "/actuator";
+    public static final String ACTUATOR_HEALTH = "actuator";
 
-    public static final String PROJECT_UPDATE_RUNNER = "/jeproject/updateRunner";
+    public static final String PROJECT_UPDATE_RUNNER = "jeproject/updateRunner";
 
-    public static final String ADD_EVENT = "/event/addEvent";
+    public static final String ADD_EVENT = "event/addEvent";
 
-    public static final String TRIGGER_EVENT = "/event/triggerEvent/";
+    public static final String TRIGGER_EVENT = "event/triggerEvent/";
 
-    public static final String UNTRIGGER_EVENT = "/event/untriggerEvent/";
+    public static final String UNTRIGGER_EVENT = "event/untriggerEvent/";
 
-    public static final String UPDATE_EVENT_TYPE = "/event/updateEventType";
+    public static final String UPDATE_EVENT_TYPE = "event/updateEventType";
 
-    public static final String DELETE_EVENT = "/event/deleteEvent";
+    public static final String DELETE_EVENT = "event/deleteEvent";
 
-    public static final String WRITE_TO_VARIABLE = "/writeVariableValue/";
+    public static final String WRITE_TO_VARIABLE = "writeVariableValue/";
 
-    public static final String GET_VARIABLE = "/getVariable/";
+    public static final String GET_VARIABLE = "getVariable/";
 
-    public static final String RUN_PROJECT_RULES = "/rule/runAllRules/";
+    public static final String RUN_PROJECT_RULES = "rule/runAllRules/";
 
-    public static final String RUN_RULE_ENGINE = "/rule/runRuleEngine/";
+    public static final String RUN_RULE_ENGINE = "rule/runRuleEngine/";
 
-    public static final String SHUT_DOWN_RULE_ENGINE = "/rule/shutDownRuleEngine/";
+    public static final String SHUT_DOWN_RULE_ENGINE = "rule/shutDownRuleEngine/";
 
 
-    public static final String UPDATE_CONFIG = "/updateConfig";
+    public static final String UPDATE_CONFIG = "updateConfig";
 
-    public static final String INFORM_USER = "/jeproject/informUser";
-    public static final String SEND_LOG = "/jeproject/sendLog";
+    public static final String INFORM_USER = "jeproject/informUser";
+    public static final String SEND_LOG = "jeproject/sendLog";
 }
