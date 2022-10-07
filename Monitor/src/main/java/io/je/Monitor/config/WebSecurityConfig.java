05bce24dd6058e3dbba23608631987243e01c8a5
kzouali <kzouali@integrationobjects.com>
2022-10-07 07:21:45 +0100

    Remove AllowUrlEncodedDoubleSlash from Spring Strict Http Firewall

--- Monitor/src/main/java/io/je/Monitor/config/WebSecurityConfig.java
+++ Monitor/src/main/java/io/je/Monitor/config/WebSecurityConfig.java
@@ -49,9 +49,6 @@ public class WebSecurityConfig {
     HttpFirewall httpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
 
-        // FIXME remove if no more needed
-        firewall.setAllowUrlEncodedDoubleSlash(true);
-
         return firewall;
     }
 
